36d34acf03e9f75a050d7d777f379811cfcdeff4
Arvey Jimenez <arve505@gmail.com>
2024-10-15 12:02:07 +0800

    update build to remove consoles.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-undef */
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { removeConsole } from 'vite-plugin-remove-console';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react()],
+    plugins: [react(), removeConsole()],
     server: {
       port: 3000,
       open: true,
