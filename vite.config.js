f31f4b51e9406a128b79104155009832a6907b24
Arvey Jimenez <arve505@gmail.com>
2024-10-15 12:15:22 +0800

    fix import issue.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { removeConsole } from 'vite-plugin-remove-console';
+import removeConsole from 'vite-plugin-remove-console';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
