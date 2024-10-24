01df6bc66316dc772d07b015c0e0785222c4b0c2
Arvey Jimenez <arve505@gmail.com>
2024-10-24 12:02:03 +0800

    update footer.

--- src/components/PreFooterSection/index.jsx
+++ src/components/PreFooterSection/index.jsx
@@ -1,4 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
+import Coat from '../../assets/images/coat.png';
+import Gown from '../../assets/images/gown.png';
+
 const PreFooter = () => {
   return (
     <div id='details' className='bg-custom-black py-[80px] px-4 w-full'>
@@ -6,10 +9,14 @@ const PreFooter = () => {
         <div className='flex-1 text-center md:text-left'>
           <div className='text-white mb-[10px] font-haviland text-[30px]'>Wedding Colors and Gifts</div>
           <div className='text-white max-w-full md:max-w-[300px] mb-[20px]'>
-            We can't wait to celebrate our special day with you! To complement the occasion, we require formal attire: a
-            black coat and tie for men, and for women, a formal dress or gown that reflects our chosen color palette.
+            We can't wait to celebrate our special day with you! To complement the occasion, we require formal attire: :
+            black coat and tie for men, and formal gown for women from any of our chosen color palette
             <br />
             <br />
+            <div className='flex flex-row items-center relative md:left-[-20px] justify-center md:justify-start mb-10'>
+              <img className='w-[100px]' src={Coat} alt='coat' />
+              <img className='w-[100px]' src={Gown} alt='coat' />
+            </div>
             <div className='inline-flex flex-row gap-5 md:mx-0 mx-auto'>
               <div className='rounded-full w-[30px] h-[30px] bg-[#cd848b]'></div>
               <div className='rounded-full w-[30px] h-[30px] bg-[#ffc097]'></div>
@@ -20,17 +27,21 @@ const PreFooter = () => {
             <br />
             Your presence is the greatest gift we could ask for, but if you'd like to offer more, a monetary gift would
             be sincerely appreciated. Thank you!
-            <br />
-            <br />
-            Your attention to detail will help make the day even more unforgettable! 🥂✨
+            {/* <br />
+            <br /> */}
+            {/* Your attention to detail will help make the day even more unforgettable! 🥂✨ */}
           </div>
         </div>
         <div className='flex-1 text-center'>
           <div className='text-white mb-[10px] font-haviland text-[30px]'>Share the Love!</div>
           <div className='text-white max-w-full md:max-w-[300px] mb-[20px] mx-auto'>
             We're so excited to share our big day with you! Join us in celebrating love, laughter, and forever. Don’t
-            forget to tag your photos with #[OurHashtag] below — we can’t wait to see all the special moments! 🥂✨
+            forget to tag your photos with #[OurHashtag] below — we can’t wait to see all the special moments!
+            <br />
+            <br />
+            Your attention to detail will help make the day even more unforgettable! 🥂✨
           </div>
+
           <div className='text-white font-bold'>#MAIpERfectmatch</div>
         </div>
         <div className='flex-1 text-center md:text-right'>
@@ -39,7 +50,7 @@ const PreFooter = () => {
             For questions or details, feel free to reach out! It's our pleasure to celebrate with you! 🥂✨
             <br />
             <br />
-            You can contact us on this following numbers:
+            You can contact us on these following numbers:
           </div>
           <div className='mb-[15px] text-white'>
             Edward:
